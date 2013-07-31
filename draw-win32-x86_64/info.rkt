f367c0c4b05b91401d68b0180b416d616b31720d
Matthew Flatt <mflatt@racket-lang.org>
2013-07-31 07:03:15 -0600

    add `pkg-desc' and `pkg-authors' to each package
    This information will be used to construct a catalog from the
    package directories.

--- draw-win32-x86_64/info.rkt
+++ draw-win32-x86_64/info.rkt
@@ -1,3 +1,7 @@
 #lang setup/infotab
 (define collection 'multi)
 (define deps '("base"))
+
+(define pkg-desc "native libraries for \"draw\" package")
+
+(define pkg-authors '(mflatt))
--- gui-win32-x86_64/info.rkt
+++ gui-win32-x86_64/info.rkt
@@ -1,3 +1,7 @@
 #lang setup/infotab
 (define collection 'multi)
 (define deps '("base"))
+
+(define pkg-desc "native libraries for \"gui\" package")
+
+(define pkg-authors '(mflatt))
