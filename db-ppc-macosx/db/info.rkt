5f391155f276da25df85081cf8c80a9760a404b0
Matthew Flatt <mflatt@racket-lang.org>
2013-09-06 21:33:50 -0600

    libsqlite3 for PPC Mac OS X

---
+++ db-ppc-macosx/db/info.rkt
@@ -0,0 +1,4 @@
+#lang setup/infotab
+(define copy-foreign-libs (quote ("libsqlite3.0.dylib")))
+(define collection 'multi)
+(define install-platform "ppc-macosx")
---
+++ db-ppc-macosx/info.rkt
@@ -0,0 +1,7 @@
+#lang setup/infotab
+(define collection 'multi)
+(define deps '("base"))
+
+(define pkg-desc "native libraries for \"base\" package")
+
+(define pkg-authors '(mflatt))
