99ad202dd97f733c1d9dc3de6cf0526febc00bd5
Matthew Flatt <mflatt@racket-lang.org>
2016-01-05 20:32:09 -0700

    libssl builds for OS X

---
+++ racket-i386-macosx-2/racket/info.rkt
@@ -0,0 +1,5 @@
+#lang setup/infotab
+
+(define install-platform "i386-macosx")
+
+(define copy-foreign-libs '("libssl.1.0.0.dylib" "libcrypto.1.0.0.dylib"))
---
+++ racket-ppc-macosx-2/racket/info.rkt
@@ -0,0 +1,5 @@
+#lang setup/infotab
+
+(define install-platform "ppc-macosx")
+
+(define copy-foreign-libs '("libssl.1.0.0.dylib" "libcrypto.1.0.0.dylib"))
