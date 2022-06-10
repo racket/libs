649494ba03af0f90bb3c8cc4511ef7458c598bf3
Matthew Flatt <mflatt@racket-lang.org>
2022-06-10 11:25:13 -0600

    upgrade more libraries
    expat-2.4.8, glib-2.72.2, pixman-0.38.4, cairo-1.16.0, jpegsrc.v9e,
    freetype-2.12.1, and gmp-6.2.1

--- racket-win32-arm64-3/racket/info.rkt
+++ racket-win32-arm64-3/racket/info.rkt
@@ -4,5 +4,4 @@
 
 (define install-platform "win32\\arm64")
 
-(define copy-foreign-libs
-  '("longdouble.dll" "ssleay32.dll" "libeay32.dll" "libiconv-2.dll"))
+(define copy-foreign-libs '("ssleay32.dll" "libeay32.dll" "libiconv-2.dll"))
